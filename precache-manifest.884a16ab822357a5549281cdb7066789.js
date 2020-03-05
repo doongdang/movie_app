ba6c5293501e022a6822ab0ac5ef69d9f2334448
CSH <doongdang@naver.com>
2020-03-05 10:43:28 +0900

    Updates

---
+++ precache-manifest.884a16ab822357a5549281cdb7066789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2251826430709ea1de4ac40f4c837728",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "65f54d3ade86c4d3785d",
+    "url": "/movie_app/static/css/main.adf50e78.chunk.css"
+  },
+  {
+    "revision": "79b3d98d9236aa532655",
+    "url": "/movie_app/static/js/2.3aa1e297.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.3aa1e297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f54d3ade86c4d3785d",
+    "url": "/movie_app/static/js/main.3930fc43.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
